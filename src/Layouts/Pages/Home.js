b8fea8190787c29f6dc7765bc2f1cf974b4e27a5
fernandocarelle <fern@ndo.codes>
2020-10-07 19:09:26 -0300

    google track

--- src/Layouts/Pages/Home.js
+++ src/Layouts/Pages/Home.js
@@ -8,10 +8,14 @@ import Team from '../../Sections/Team'
 /* import Appointment from '../../Sections/Appointment'
 import Blog from './Blog' */
 import Contact from '../../Sections/Contact'
-
 import Highlights from '../../Sections/Highlights';
-
 import Whatsapp from '../../Components/Whatsapp';
+
+import { withRouter } from "react-router-dom";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_CODE);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const home = props => {
     return (
@@ -30,4 +34,4 @@ const home = props => {
     )
 }
 
-export default home
+export default withRouter(home)
