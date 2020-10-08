ffa4f3a5ab903110e0b46e7541d9605adc8745f3
fernandocarelle <fern@ndo.codes>
2020-10-07 21:36:31 -0300

    email

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -20,7 +20,7 @@ function Contact() {
     function send() {
         const formData = new FormData();
         Object.keys(campos).forEach(key => formData.append(key, campos[key]));
-        axios.post('https://api-email-divas.herokuapp.com/send',
+        axios.post('https://api-email-divas.herokuapp.com/',
             formData,
             {
                 headers: {
