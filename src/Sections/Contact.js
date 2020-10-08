baa2bcce8b59642ba0d99986de6d6d8897b069cb
fernandocarelle <fern@ndo.codes>
2020-10-07 21:16:47 -0300

    email

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -1,5 +1,5 @@
-import React, { Component, useState } from 'react'
-import axios from '../axios-orders'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 function Contact() {
 
